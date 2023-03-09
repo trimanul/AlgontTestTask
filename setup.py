a73cb71c861a08fe2684c2c6f6369c4dbbb512fd
trimanul <ivanko150702@gmail.com>
2023-03-09 12:39:38 +0300

    added setup logs

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@ import sqlalchemy as sa
 import json
 from utils.utils import load_config
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+logger.debug("Loading config...")
 config = load_config()["db_config"]
+logger.debug("Done")
+
+logger.debug(f"Creating {config["db_engine"]} database {config["db_path"]}")
 
 db_url = sa.URL.create(
             f'{config["db_engine"]}+{config["db_dialect"]}',
             database=config["db_path"])
+
 
 engine = sa.create_engine(db_url)
 
@@ -21,4 +30,5 @@ cpu_load_table = sa.Table(
 )
 
 meta.create_all(engine)
-
+logger.debug("Done")
+logger.debug("Setup finished")
