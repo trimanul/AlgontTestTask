939995bde6c84574ed2afe50ba3a2669ae173ae6
trimanul <ivanko150702@gmail.com>
2023-03-09 11:05:15 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
 import sqlalchemy as sa
 import json
 from utils.utils import load_config
-from setuptools import setup
-
-setup(
-        name="algont_test_kudinov",
-        version="0.5.0",
-        install_requires=[
-                'Flask == 2.2.3',
-                'SQLAlchemy == 2.0.2',
-                'psutil == 5.6.7',
-                'importlib-metadata; python_version == "3.11"',
-        ],
-)
-
 
 config = load_config()["db_config"]
 
